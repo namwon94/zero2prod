87f4532d430d6b596e8ea30739341ee971e72a33
namwonkim <namwon94@gamil.com>
2025-01-31 17:23:02 +0900

    modified:   Cargo.lock 	modified:   Cargo.toml 	new file:   src/domain.rs 	modified:   src/lib.rs 	modified:   src/routes/subscriptions.rs 	modified:   tests/health_check.rs 	modified:   zz_study.txt

---
+++ src/domain.rs
@@ -0,0 +1,92 @@
+//'String'과 '&str'에 'graphemes' 메서드를 제공하기 위한 확장 트레이트
+use unicode_segmentation::UnicodeSegmentation;
+
+pub struct NewSubscriber {
+    pub email: String,
+    pub name: SubscriberName
+}
+/*
+    튜플 구조체 : String 타입의 단일 필드(이름이 없는)를 갖는 새로운 타입 
+        -> String에 사용할 수 있는 어떤 메서드도 상속하지 않으며, 타입 변수에 String을 할당하려하면 컴파일 오류남
+*/
+#[derive(Debug)]
+pub struct SubscriberName(String);
+
+impl SubscriberName {
+    //입력이 subscriber 이름에 대한 검증 조건을 모두 만족하면 'SubscriberName' 인스턴스를 반환한다.
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
+        //'.trim()'은 입력 's'에 대해 뒤로 계속되는 공백 문자가 없는 뷰를 반환한다.
+        //'.is_empty'는 해당 뷰가 문자를 포함하고 있는지 확인한다.
+        let is_empty_or_whitespace = s.trim().is_empty();
+        //grapheme는 "사용자가 인지할 수 있는" 문자로서 유니코드 표준에 의해 정의된다.
+        //'a'는 단일 grapheme이지만, 두 개의 문자가 조합된 것이다. (a 와 *)
+        //grapheme 입력 's'안의 grapheme에 대한 이터레이터를 반환한다.
+        //'true'는 우리가 확장된 grapheme 정의 셋, 즉 권장되는 정의 셋을 사용하기 원함을 의미한다.
+        let is_too_long = s.graphemes(true).count() > 256;
+
+        //입력 's'의 모든 문자들에 대해 반복하면서 forbidden 배열  안에 있는 문자 중, 어느 하나와 일치하는 문자가 있는지 확인한다.
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = s
+            .chars()
+            .any(|g| forbidden_characters.contains(&g));
+        
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            // 'panic'을 'Err()'으로 치환한다.
+            //panic!("{} is not a valid subscriber name", s)
+            Err(format!("{} is not a valid subscriber name.", s))
+        }else {
+            Ok(Self(s))
+        }
+        
+    }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "ë".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_grapheme_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}',] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
+    }
+}
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@ use sqlx::PgPool;
 use chrono::Utc;
 //use tracing::Instrument;
 use uuid::Uuid;
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -22,11 +23,18 @@ pub struct FormData {
 )]
 
 //항상 200 OK를 반환시킴
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>, 
+    pool: web::Data<PgPool>
+) -> HttpResponse {
+    //'web::Form'은 'FormData'의 래퍼이다. 'form.0'을 사용하면 기반 'FormData'에 접근할 수 있다.
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name).expect("Name validation failed.")
+    };
     //'Result'는 'Ok'와 'Err'라는 두개의 변형(variant)를 갖는다.(성공과 실패 의미)
     //'match' 구문을 사용해서 결과에 따라 무엇을 수행할지 선택한다.
-    match insert_subscriber(&pool, &form).await
-    {
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => {
             tracing::info!("New subscriber details have been saved");
             HttpResponse::Ok().finish()
@@ -39,15 +47,38 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         }
     }
 }
+//입력이 subscriber 이름에 대한 검증 제약 사항을 모두 만족하면 'true'를 반환한다.
+//그렇지 않으면 'false'를 반환한다.
+/* 
+pub fn is_valid_name(s: &str) -> bool {
+    //'.trim()'은 입력 's'에 대해 뒤로 계속되는 공백 문자가 없는 뷰를 반환한다.
+    //'.is_empty'는 해당 뷰가 문자를 포함하고 있는지 확인한다.
+    let is_empty_or_whitespace = s.trim().is_empty();
+    //grapheme는 "사용자가 인지할 수 있는" 문자로서 유니코드 표준에 의해 정의된다.
+    //'a'는 단일 grapheme이지만, 두 개의 문자가 조합된 것이다. (a 와 *)
+    //grapheme 입력 's'안의 grapheme에 대한 이터레이터를 반환한다.
+    //'true'는 우리가 확장된 grapheme 정의 셋, 즉 권장되는 정의 셋을 사용하기 원함을 의미한다.
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    //입력 's'의 모든 문자들에 대해 반복하면서 forbidden 배열  안에 있는 문자 중, 어느 하나와 일치하는 문자가 있는지 확인한다.
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s
+        .chars()
+        .any(|g| forbidden_characters.contains(&g));
+
+    //어떤 한 조건이라도 위반하면 'false'를 반환한다.
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
+}
+*/
 
 #[tracing::instrument(
     name = "Saving new subscriber details int the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
 
 pub async fn insert_subscriber(
     pool: &PgPool,
-    form: &FormData
+    new_subscriber: &NewSubscriber
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
@@ -55,8 +86,9 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        // 'as_ref'를 사용한다.
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
